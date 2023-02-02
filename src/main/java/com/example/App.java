be0a475b35186953d63c922e30759b78e32312b3
Luca <luca.cavagnoli@gmail.com>
2023-02-02 07:27:05 +0100

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.jobs.details.JobDetailsAsmGenerator;
+import org.jobrunr.scheduling.BackgroundJob;
+import org.jobrunr.storage.InMemoryStorageProvider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+
+      JobRunr.configure()
+      // .useJobActivator(applicationContext::getBean)
+      .useStorageProvider(new InMemoryStorageProvider())
+      .useJobDetailsGenerator(new JobDetailsAsmGenerator())
+      .useBackgroundJobServer()
+      .initialize();
+
+      BackgroundJob.enqueue(() -> System.out.println("%s enqueue".formatted(Instant.now())));
+
+      BackgroundJob.schedule(UUID.randomUUID(), Instant.now().plusMillis(500), () -> System.out.println("%s schedule".formatted(Instant.now())));
+
+      BackgroundJob.scheduleRecurrently("a", Duration.ofSeconds(5), () -> System.out.println("%s scheduleRecurrently(Duration)".formatted(Instant.now())));
+
+      BackgroundJob.scheduleRecurrently("b", "*/5 * * * * *", () -> System.out.println("%s scheduleRecurrently(Cron)".formatted(Instant.now())));
+
+      System.in.read();
+    }
+}
